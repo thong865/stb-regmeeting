8ed3e73c4a472f3ff4cb2266e1111bf2e5f2fed8
singhaltd <85279370+singhaltd@users.noreply.github.com>
2023-03-30 14:40:35 +0700

    settup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <div>
+    <div class="flex h-screen w-screen">
+      <div> <video ref="video" width="300" height="300"></video>
+        <canvas ref="canvas" style="display: none"></canvas>
+        <div v-if="qrCode">
+          <p>QR Code: {{ qrCode }}</p>
+        </div>
+      </div>
+      <div>
+        asdkjfk
+      </div>
 
+    </div>
+  </div>
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QrCode from "qrcode-reader";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  data() {
+    return {
+      qrCode: null,
+    };
+  },
+  mounted() {
+    this.initCamera();
+  },
+  methods: {
+    initCamera() {
+      const video = this.$refs.video;
+      const canvas = this.$refs.canvas;
+      const context = canvas.getContext("2d");
+      const qr = new QrCode();
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+      navigator.mediaDevices
+        .getUserMedia({
+          video: { facingMode: "environment" },
+        })
+        .then((stream) => {
+          video.srcObject = stream;
+          video.play();
+
+          requestAnimationFrame(tick);
+        });
+
+      function tick() {
+        if (video.readyState === video.HAVE_ENOUGH_DATA) {
+          canvas.width = video.videoWidth;
+          canvas.height = video.videoHeight;
+          context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+          const imageData = context.getImageData(
+            0,
+            0,
+            canvas.width,
+            canvas.height
+          );
+          qr.callback = (error, result) => {
+            if (error) {
+              console.error(error);
+            } else {
+              this.qrCode = result.result;
+            }
+          };
+          qr.decode(imageData);
+        }
+
+        requestAnimationFrame(tick);
+      }
+    },
+  },
+};
+</script>
